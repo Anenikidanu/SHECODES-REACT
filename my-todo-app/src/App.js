0bc510724282c2d4f1a67de832cee356fb2e9280
aneni <aneni.kidanu@aastustudent.edu.et>
2026-03-04 09:03:46 +0300

    Added my todo app

---
+++ my-todo-app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to My To-Do App</h1>
+      <p>Stay organized. Stay productive.</p>
+    </div>
+  );
+}
+
+export default App;
